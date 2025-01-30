ee47ccf9aef342e912cada44bcc45cd2bc62478e
Hernâni Silva <hcsilvamatos@hotmail.com>
2025-01-30 19:45:53 +0000

    primary styling of the forms

--- src/pages/LogInPage.jsx
+++ src/pages/LogInPage.jsx
@@ -1,7 +1,34 @@
+import axios from "axios";
+import { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "../contexts/auth.context";
+
 const LogInPage = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
   return (
     <div>
       <h1>Log in page</h1>
+      <form className="form">
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <button>Login</button>
+      </form>
     </div>
   );
 };
