3b84261874b3419a68ad852929f2b73f3ef26521
Luke Farrell <lukefarrell2012@gmail.com>
2025-01-30 13:57:59 +0000

    error fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css";
+import React from 'react'
 import HomePage from "./pages/HomePage";
 import LogInPage from "./pages/LogInPage";
 import SignUpPage from "./pages/SignUpPage";
@@ -10,40 +10,41 @@ import { Routes, Route } from "react-router-dom";
 import ChatWidget from "./components/ChatWidget";
 
 function App() {
-  return (
-    <div className="h-screen flex flex-col">
-      {/* Navbar - fixed at top */}
-      <nav className="h-[60px] bg-red-200 w-full">
-        <Navbar />
-      </nav>
-      
-      {/* Middle section container */}
-      <div className="flex flex-1 min-h-0"> {/* min-h-0 prevents overflow */}
-        {/* Sidebar - between navbar and footer */}
-        <aside className="w-[200px] bg-blue-200">
-          Sidebar area
-        </aside>
+    return (
+      <div className="h-screen flex flex-col">
+        {/* Navbar */}
+        <nav className="h-[60px] bg-red-200 shrink-0">
+          <Navbar />
+        </nav>
+        
+        {/* Middle section */}
+        <div className="flex flex-1">
+          <aside className="w-64 bg-blue-200">
+            Sidebar area
+          </aside>
 
-        {/* Main content - takes remaining width */}
-        <main className="flex-1 bg-green-200">
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/user" element={<UserPage />} />
-            <Route path="/sign-up" element={<SignUpPage />} />
-            <Route path="/log-in" element={<LogInPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-        </main>
+          <main className="flex-1 bg-green-200">
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/user" element={<UserPage />} />
+              <Route path="/sign-up" element={<SignUpPage />} />
+              <Route path="/log-in" element={<LogInPage />} />
+              <Route path="*" element={<NotFoundPage />} />
+            </Routes>
+          </main>
+        </div>
+
+        {/* Footer */}
+        <footer className="h-[60px] bg-yellow-200 shrink-0">
+          <Footer />
+        </footer>
+
+        {/* Chat Widget */}
+        <div className="fixed bottom-16 right-4 z-50">
+          <ChatWidget />
+        </div>
       </div>
-
-      {/* Footer - fixed at bottom */}
-      <footer className="h-[60px] bg-yellow-200 w-full">
-        <Footer />
-      </footer>
-
-      <ChatWidget />
-    </div>
-  );
+    );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import "./styles/index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import './index.css';
+import './styles/App.css';
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
