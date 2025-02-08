e3731c003c34976c6d2edc70581a7b9b88e79a29
Hernâni Silva <hcsilvamatos@hotmail.com>
2025-02-08 16:00:34 +0000

    created the update password form and the logic behind it

--- src/components/PasswordChange.jsx
+++ src/components/PasswordChange.jsx
@@ -1,12 +1,36 @@
+import axios from "axios";
 import { useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+
+const webToken = localStorage.getItem("authToken");
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:5005";
 
 const PasswordChange = () => {
+  const { userId } = useParams();
   const [currentPassword, setCurrentPassword] = useState("");
   const [newPassword, setNewPassword] = useState("");
   const [newPasswordConf, setNewPasswordConf] = useState("");
+  const nav = useNavigate();
+  async function handlePasswordChange(e) {
+    e.preventDefault();
+    if (newPassword === newPasswordConf) {
+      try {
+        const newPasswords = {
+          old: currentPassword,
+          new: newPassword,
+        };
+        const responseToChange = await axios.patch(
+          `${BACKEND_URL}/users/update-user/${userId}/password-change`,
+          newPasswords,
+          { headers: { authorization: `Bearer ${webToken}` } }
+        );
 
-  async function handlePasswordChange() {
-    
+        alert("password Changed with sucess!");
+        nav(`/users/user/${userId}`);
+      } catch (error) {
+        console.log(error);
+      }
+    }
   }
   return (
     <div>
@@ -20,12 +44,20 @@ const PasswordChange = () => {
           />
         </label>
         <label>
-            New Password:
-            <input type="password" value={newPassword} onChange={(e)=> setNewPassword(e.target.value)}/>
+          New Password:
+          <input
+            type="password"
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
+          />
         </label>
         <label>
-           Confirm New Password:
-            <input type="password" value={newPasswordConf} onChange={(e)=> setNewPasswordConf(e.target.value)}/>
+          Confirm New Password:
+          <input
+            type="password"
+            value={newPasswordConf}
+            onChange={(e) => setNewPasswordConf(e.target.value)}
+          />
         </label>
         <button>Change Password</button>
       </form>
