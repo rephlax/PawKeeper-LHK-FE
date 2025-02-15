1b52d2642e92214fa51253944733092fcd13a1ae
Hernâni Silva <hcsilvamatos@hotmail.com>
2025-02-15 15:18:47 +0000

    minor change on the update user form

--- src/components/UpdateUserForm.jsx
+++ src/components/UpdateUserForm.jsx
@@ -184,7 +184,7 @@ const UpdateUserForm = () => {
           />
         </label>
 
-        <button type="submit">{t('userupdate.submit')}</button>
+        <button type="submit">{t('userupdate.submitButton')}</button>
         <Link to={`/users/user/${userId}`}>
           <button>{t('userupdate.backbutton')}</button>
         </Link>
