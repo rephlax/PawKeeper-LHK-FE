7b6a7e67fbc1a5b606bf3c32c37ccd14a7719956
Luke Farrell <lukefarrell2012@gmail.com>
2025-02-20 12:01:39 +0000

    page wrapper padding removal

--- src/components/PageWrapper.jsx
+++ src/components/PageWrapper.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PageWrapper = ({ children }) => (
   <div className='flex min-h-screen bg-cream-50/50'>
-    <div className='flex-1 flex flex-col justify-center items-center py-12 px-4'>
+    <div className='flex-1 flex flex-col justify-center items-center'>
       <div className='w-full max-w-3xl mx-auto'>{children}</div>
     </div>
   </div>
