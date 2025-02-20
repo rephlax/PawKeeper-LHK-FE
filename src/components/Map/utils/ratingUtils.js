640b8c3187d987c7df24e71b30415b51ab250346
Hernâni Silva <hcsilvamatos@hotmail.com>
2025-02-20 19:18:55 +0000

    refactoring the ratingUtils file

--- src/components/Map/utils/ratingUtils.js
+++ src/components/Map/utils/ratingUtils.js
@@ -1,11 +1,23 @@
-export const calculateAverageRating = (pin) => {
-    if (!pin?.user.reviewsReceived || !Array.isArray(pin.user.reviewsReceived) || pin.user.reviewsReceived.length === 0) {
-      return 0;
-    }
-  
-    const totalRating = pin.user.reviewsReceived.reduce((sum, review) => sum + review.rating, 0);
-    const averageRating = totalRating / pin.user.reviewsReceived.length;
-  
-    // Round to 1 decimal place
-    return Math.round(averageRating * 10) / 10;
-  };
+import axios from "axios";
+
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
+
+export async function calculateAverageRating(pin) {
+  try {
+    const user = await axios.get(`${BACKEND_URL}/users/user/${pin.user._id}`)
+
+  if (!user.reviewsReceived || !Array.isArray(user.reviewsReceived) || user.reviewsReceived.length === 0) {
+    return 0;
+  }
+
+  const totalRating = user.reviewsReceived.reduce((sum, review) => sum + review.rating, 0);
+  const averageRating = totalRating / user.reviewsReceived.length;
+
+  // Round to 1 decimal place
+  return Math.round(averageRating * 10) / 10;
+  } catch (error) {
+    console.error("Error fetching user data or calculating rating:", error);
+    return 0;
+  }
+} 
+
