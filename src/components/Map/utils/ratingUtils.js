069e86caa252ffc99fe43ffd0afef302bdced25e
Hernâni Silva <hcsilvamatos@hotmail.com>
2025-02-20 19:22:40 +0000

    minor change in the utils file

--- src/components/Map/utils/ratingUtils.js
+++ src/components/Map/utils/ratingUtils.js
@@ -1,23 +1,32 @@
-import axios from "axios";
+import axios from 'axios'
 
 const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
+const webToken = localStorage.getItem('authToken')
 
 export async function calculateAverageRating(pin) {
   try {
-    const user = await axios.get(`${BACKEND_URL}/users/user/${pin.user._id}`)
+    const user = await axios.get(`${BACKEND_URL}/users/user/${pin.user._id}`, {
+      headers: { authorization: `Bearer ${webToken}` },
+    })
 
-  if (!user.reviewsReceived || !Array.isArray(user.reviewsReceived) || user.reviewsReceived.length === 0) {
-    return 0;
-  }
+    if (
+      !user.reviewsReceived ||
+      !Array.isArray(user.reviewsReceived) ||
+      user.reviewsReceived.length === 0
+    ) {
+      return 0
+    }
 
-  const totalRating = user.reviewsReceived.reduce((sum, review) => sum + review.rating, 0);
-  const averageRating = totalRating / user.reviewsReceived.length;
+    const totalRating = user.reviewsReceived.reduce(
+      (sum, review) => sum + review.rating,
+      0,
+    )
+    const averageRating = totalRating / user.reviewsReceived.length
 
-  // Round to 1 decimal place
-  return Math.round(averageRating * 10) / 10;
+    // Round to 1 decimal place
+    return Math.round(averageRating * 10) / 10
   } catch (error) {
-    console.error("Error fetching user data or calculating rating:", error);
-    return 0;
+    console.error('Error fetching user data or calculating rating:', error)
+    return 0
   }
-} 
-
+}
